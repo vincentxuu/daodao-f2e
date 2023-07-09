e35cc3d3f141c9649951a38bf8acdac3b31075de
vincent <innocent0705@gmail.com>
2023-07-09 12:45:50 +0800

    update hamberger UserAvatar

--- shared/components/Navigation_v2/MainNav/Hamberger/UserAvatar/index.jsx
+++ shared/components/Navigation_v2/MainNav/Hamberger/UserAvatar/index.jsx
@@ -4,9 +4,8 @@ import Link from 'next/link';
 import { Avatar, Box, IconButton, Menu, MenuItem } from '@mui/material';
 import { Group } from '@mui/icons-material';
 import { useRouter } from 'next/router';
-import useFirebase from '../../../../../../hooks/useFirebase';
 import { useDispatch, useSelector } from 'react-redux';
-
+import useFirebase from '../../../../../../hooks/useFirebase';
 
 const UserAvatar = ({ onCloseMenu }) => {
   const { push } = useRouter();
@@ -14,11 +13,33 @@ const UserAvatar = ({ onCloseMenu }) => {
   const [isOpenMenu, setIsOpenMenu] = useState(null);
   const user = useSelector((state) => state.user);
 
-  if (user.initialState.email === '') {
+  if (user?.email) {
+    return (
+      <IconButton
+        sx={{
+          margin: '0 10px',
+          display: 'flex',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+        }}
+        onClick={() => {
+          onCloseMenu();
+          push('/profile');
+        }}
+      >
+        <Avatar
+          alt={user?.name ?? ''}
+          src={user?.photoURL ?? ''}
+          // onClick={(event) => setIsOpenMenu(event.currentTarget)}
+        />
+      </IconButton>
+    );
+  } else {
     return (
       <IconButton
         sx={{ margin: '0 10px', fontSize: '16px', color: 'white' }}
         onClick={() => {
+          onCloseMenu();
           push('/login');
         }}
       >
@@ -26,69 +47,6 @@ const UserAvatar = ({ onCloseMenu }) => {
       </IconButton>
     );
   }
-  return (
-    <IconButton
-      sx={{
-        margin: '0 10px',
-        display: 'flex',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-      }}
-      onClick={() => {
-        onCloseMenu();
-        push('/profile');
-      }}
-    >
-      <Avatar
-        alt={user?.name ?? ''}
-        src={user?.photoURL ?? ''}
-        // onClick={(event) => setIsOpenMenu(event.currentTarget)}
-      />
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'flex-start',
-          alignItems: 'center',
-          marginLeft: '20px',
-        }}
-      >
-        <Box
-          sx={{
-            margin: '0 10px',
-            fontSize: '26px',
-            color: 'white',
-            fontWeight: '500',
-          }}
-        >
-          Points: 0
-        </Box>
-        <img src="/assets/point.png" alt="point" width={26} height={26} />
-      </Box>
-      {/* <Menu
-        id="user-menu"
-        anchorEl={isOpenMenu}
-        open={Boolean(isOpenMenu)}
-        onClose={() => setIsOpenMenu(false)}
-      >
-        <MenuItem
-          onClick={() => {
-            setIsOpenMenu(false);
-            push("/myisland");
-          }}
-        >
-          我的島島
-        </MenuItem>
-        <MenuItem
-          onClick={() => {
-            signOutWithFacebook();
-            setIsOpenMenu(false);
-          }}
-        >
-          登出
-        </MenuItem> */}
-      {/* </Menu> */}
-    </IconButton>
-  );
 };
 
 export default UserAvatar;
